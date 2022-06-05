1c2773afd95360db1186dd2fd8da03ed00c89118
Isaac <isaacbarros88@gmail.com>
2022-06-05 15:41:34 -0300

    Days 001 and 002

---
+++ day001.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+
+
+var str =  "Hello, playground"
+
+str = "goodbye"
+
+//print(str)
+
+
+
+var str1 = """
+
+    This goes
+
+    over multiple
+
+    lines
+
+    """
+
+var score = 85
+
+var str3 = "Your score was \(score)"
+
+print(str3)
+
+
+
+//constantes 
+
+let taylor = "Swift"
+
